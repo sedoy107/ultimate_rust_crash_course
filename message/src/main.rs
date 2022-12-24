5c113e194dccbf75d0e14a56a8a99843303297e8
Sergey Gorbov <sergey.gorbov@aon.com>
2022-12-24 13:18:55 -0600

    My code - messing around

--- message/src/main.rs
+++ message/src/main.rs
@@ -1,3 +1,5 @@
+use message::greet;
+
 fn main() {
     let mut my_val = 16;
     my_val = 2;
@@ -5,4 +7,6 @@ fn main() {
     const MY_INT64: i64 = 100500;
 
     println!("Hello, world {my_val}, {MY_INT64}!");
+
+    greet();
 }
