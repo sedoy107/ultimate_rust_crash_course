3a104b63e3c75ced922d028f27792a0ab3b15be4
Sergey Gorbov <sergey.gorbov@aon.com>
2023-01-21 13:17:38 -0600

    Exercise G

--- exercise/g_collections_enums/src/main.rs
+++ exercise/g_collections_enums/src/main.rs
@@ -8,6 +8,11 @@
 // - `Hit`, containing the distance from the center (an f64)
 // - `Miss`
 //
+enum Shot {
+    Bullseye,
+    Hit(f64),
+    Miss,
+}
 // You will need to complete 1b as well before you will be able to run this program successfully.
 
 impl Shot {
@@ -18,6 +23,12 @@ impl Shot {
         // - return 2 points if `self` is a `Shot::Hit(x)` where x < 3.0
         // - return 1 point if `self` is a `Shot::Hit(x)` where x >= 3.0
         // - return 0 points if `self` is a Miss
+        match self {
+            Shot::Bullseye => 5,
+            Shot::Hit(x) if x < 3.0 => 2,
+            Shot::Hit(_) => 1,
+            Shot::Miss => 0,
+        }
     }
 }
 
@@ -34,10 +45,21 @@ fn main() {
     //      - Less than 1.0 -- `Shot::Bullseye`
     //      - Between 1.0 and 5.0 -- `Shot::Hit(value)`
     //      - Greater than 5.0 -- `Shot::Miss`
+    for coord in arrow_coords {
+        coord.print_description();
+        shots.push(match coord.distance_from_center() {
+            x if x < 1.0 => Shot::Bullseye,
+            x if x < 5.0 => Shot::Hit(x),
+            _ => Shot::Miss,
+        });
+    }
 
 
     let mut total = 0;
     // 3. Finally, loop through each shot in shots and add its points to total
+    for shot in shots {
+        total += shot.points();
+    }
 
     println!("Final point total is: {}", total);
 }
